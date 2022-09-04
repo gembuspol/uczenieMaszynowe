bc6092c1af2ccf6a59648aa0a3beef896a370159
gembuspol <85030649+gembuspol@users.noreply.github.com>
2022-09-04 21:36:04 +0200

    Add files via upload

---
+++ cwiczenia6_zadanie2_obraz07.pl
@@ -0,0 +1,27 @@
+przedmiot(stolik).
+przedmiot(fotel).
+przedmiot(pilot).
+przedmiot(podkladka).
+przedmiot(komoda).
+przedmiot(wiezaHifi).
+przedmiot(plytaCD).
+przedmiot(stolikZszuflada).
+przedmiot(szchownica).
+przedmiot(sluchawki).
+przedmiot(czapka).
+przedmiot(kielich).
+przedmiot(gazeta).
+przedmiot(dywan).
+stoiNa(dywan,stolik).
+stoiNa(dywan, fotel).
+stoiNa(fotel,sluchawki).
+stoiNa(fotel,czapka).
+stoiNa(stolik,pilot).
+stoiNa(stolik,podkladka).
+stoiNa(stolik,kielich).
+stoiNa(stolik,gazeta).
+stoiNa(stolikZszuflada,szachownica).
+kolor(stolik,czarny).
+kolor(fotel,brazowy).
+kolor(komoda,brazowy).
+kolor(stolikZszuflada,brazowy).
